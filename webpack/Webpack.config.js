64e16a5cd0846a061d5774f98d9d56f7d379b02a
Arthurbhs <arthur.shekinarfoxy@gmail.com>
2022-11-28 11:38:27 -0300

    inserindo imagem

--- webpack/Webpack.config.js
+++ webpack/Webpack.config.js
@@ -1,12 +1,24 @@
+const modoDev = process.env.NODE_ENV !== 'production'
 const webpack = require('webpack')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const UglifyJsPlugin = require('uglifyjs--webpack-plugin')
+const OptimizeCSSAssentsPlugin = require('optimize-css-assets-webpack-plugin')
 
 module.exports = {
-    mode: 'development',
+    mode: modoDev ? 'development' : 'production',
     entry: './src/Principal',
     output : {
         filename: 'principal.js',
         path: __dirname + '/public'
+    },
+    optimization: {
+        minimizer: [
+            new UglifyJsPlugin({
+                cache: true,
+                parallel: true 
+            }),
+            new OptimizeCSSAssentsPlugin({})
+        ]
     },
 
     plugins: [
@@ -25,6 +37,9 @@ new MiniCssExtractPlugin({
                 'sass-loader',
 
             ]
+        }, {
+            test: /\.(png|svg|jpg|gif)$/,
+            use: ['fle-loader']
         }]
     }
 }
